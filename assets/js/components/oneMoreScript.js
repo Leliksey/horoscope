c2887b1f9e56490f82b65c1c696a173610cfc1f0
Leliksey <captainfroody@gmail.com>
2023-12-13 15:25:18 +0200

    reviews

--- assets/js/components/oneMoreScript.js
+++ assets/js/components/oneMoreScript.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     $(document).on("click", ".overlay", function() {
         $(".overlay").remove();
         $(".burgerMenu__close").click();
+        $(".review").hide();
     });
     $("#seeMore").click(function(e) {
         e.preventDefault();
@@ -19,5 +20,32 @@ $(document).ready(function() {
     $(".pricing__tab").click(function() {
         $(".pricing__tab").removeClass("active");
         $(this).addClass("active");
-    })
+    });
+    $(".reviews__item-img_play, .reviews .link-more").click(function() {
+        $("body").append(overlay);
+        $(".review").show();
+    });
+    
+    $('.reviews__items').owlCarousel({
+        loop:true,
+        nav:true,
+        mouseDrag:true,
+        touchDrag: true,
+        pullDrag:false,
+        freeDrag:false,
+        URLhashListener:false,
+        dots:true,
+        autoplay:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            1400:{
+                items:3
+            }
+        }
+    });
 });
